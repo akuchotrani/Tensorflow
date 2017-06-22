35e51ea95f9c8ef69e662e6147b21769ab2b325a
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-06-22 15:18:21 -0700

    tensorflow getting started

---
+++ Tensorflow Core.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 22 14:46:16 2017
+
+@author: aakash.chotrani
+"""
+
+import tensorflow as tf
+
+node1 = tf.constant(3.0, dtype = tf.float32)
+node2 = tf.constant(4.0)
+print(node1,node2)
