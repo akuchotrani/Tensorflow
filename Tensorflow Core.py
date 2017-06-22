d6aba5c321576df23f852dbc91ebd8f509bdbd78
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-06-22 15:31:37 -0700

    adding nodes

--- Tensorflow Core.py
+++ Tensorflow Core.py
@@ -10,3 +10,13 @@ import tensorflow as tf
 node1 = tf.constant(3.0, dtype = tf.float32)
 node2 = tf.constant(4.0)
 print(node1,node2)
+
+
+#creating a session to evaluate the nodes
+sess = tf.Session()
+print(sess.run([node1,node2]))
+
+#adding two nodes together
+node3 = tf.add(node1,node2)
+print("node3: ",node3)
+print("sess.run(node3): ",sess.run(node3))
